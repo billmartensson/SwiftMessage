480cf2a443a21e21bef40051dc912165dcb469a2
Bill Mårtensson <bill@magictechnology.se>
2020-12-18 11:06:55 +0100

    Initial Commit

---
+++ MyMessage/MyMessageApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMessageApp.swift
+//  MyMessage
+//
+//  Created by Bill Martensson on 2020-12-18.
+//
+
+import SwiftUI
+
+@main
+struct MyMessageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
